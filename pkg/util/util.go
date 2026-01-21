88fd5ef9c6f21b4969150109eb0bfef051e75e06
Ivan Mikheykin <ivan.mikheykin@flant.com>
2026-01-21 16:22:48 +0300

    chore: add heritage=deckhouse label for Pods in user ns (#27)
    * chore: add heritage=deckhouse label for Pods in user ns
    
    Support security hardening for Deckhouse system components implemented by https://github.com/deckhouse/deckhouse/pull/16749

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -209,6 +209,7 @@ func SetRecommendedLabels(obj metav1.Object, installerLabels map[string]string, 
 	staticLabels := map[string]string{
 		common.AppKubernetesManagedByLabel: controllerName,
 		common.AppKubernetesComponentLabel: "storage",
+		common.HeritageLabel:               common.HeritageValue,
 	}
 
 	// Merge static & existing labels
