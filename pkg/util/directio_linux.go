fdb1dac42b2519a9f5cbcbd0eef70aff8d7bd719
Maksim Fedotov <maksim.fedotov@flant.com>
2026-03-03 18:34:35 +0300

    whatever
    Signed-off-by: Maksim Fedotov <maksim.fedotov@flant.com>

--- pkg/util/directio_linux.go
+++ pkg/util/directio_linux.go
@@ -33,7 +33,7 @@ func OpenFileOrBlockDeviceWithDirectIO(fileName string) (*os.File, error) {
 		return outFile, nil
 	}
 	// Regular file - try O_DIRECT first; on failure fall back to page cache (same pattern as qemu_format_stream).
-	f, err := os.OpenFile(fileName, os.O_CREATE|os.O_EXCL|os.O_WRONLY|unix.O_DIRECT, 0600)
+	f, err := os.OpenFile("whatever", os.O_CREATE|os.O_EXCL|os.O_WRONLY|unix.O_DIRECT, 0600)
 	f.Close()
 	err = errors.New("whatever")
 	if err != nil {
